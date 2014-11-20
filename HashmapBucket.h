56c5d1d6433584e38deaf7e9c6086e163984dc72
Joshua Lee Kegley <josh1289op@gmail.com>
2014-11-19 22:02:02 -0500

    adding bucket support, insert, remove, and search and test cases

--- HashmapBucket.h
+++ HashmapBucket.h
@@ -29,19 +29,23 @@ public:
 	}
 
 	bool insert(int key, char value){//insert a value into the map
-		int position = hash(key, 0);
+		int position = hash(key);
 		Node* current = map[position];
 		if (current == NULL){//IF BUCKET COMPLETELY EMPTY!!!!
-			current = new Node;
+			current = new Node();
 			current->key = key;
 			current->value = value;
+			map[position] = current;
+			++SIZE;
+			return true;
 		}
 		else{
+			int i = 1;
 			while (current->next != NULL && current->key != key){
 				current = current->next;
 			}
 			if (current->next == NULL){//if no matching key was found, create a new link
-				current->next = new Node;
+				current->next = new Node();
 				current = current->next;
 				current->key = key;
 				current->value = value;
@@ -49,15 +53,55 @@ public:
 			else{//if matching key was found, replace the value. 
 				current->value = value;
 			}
+			++SIZE;
 			return true;
 		}
 	}
 
 	bool remove(int key, char &value){//remove an item from the map
-
+		int position = hash(key);
+		Node* current = map[position];
+		Node* temp;
+		if (current->key == key){
+			value = current->value;
+			temp = current->next;
+			delete current;
+			map[position] = temp;
+			--SIZE;
+			return true;
+		}
+		while (current->next != NULL){
+			if (current->next->key == key){
+				temp = current->next;
+				value = temp->value;
+				temp = temp->next;
+				delete current->next;
+				current->next = temp;
+				--SIZE;
+				return true;
+			}
+			current = current->next;
+		}
+		return false;
 	}
 	bool search(int key, char &value){//search for a value in the map
-		
+		int position = hash(key);
+		Node * current = map[position];
+		Node* current = map[position];
+		Node* temp;
+		if (current->key == key){
+			value = current->value;
+			return true;
+		}
+		while (current->next != NULL){
+			if (current->next->key == key){
+				temp = current->next;
+				value = temp->value;
+				return true;
+			}
+			current = current->next;
+		}
+		return false;
 	}
 	void clear(){//clear the whole map
 	
@@ -80,14 +124,24 @@ public:
 	}
 
 	std::ostream& print(std::ostream& out) const {
-
+		Node * current;
+		for (int i = 0; i < CAPACITY; ++i){
+			current = map[i];
+			out << "Bucket: " << i << " - ";
+			while (current != NULL){
+				out << "  [Key: " << current->key << " Value: " << current->value << "]  - ";
+				current = current->next;
+			}
+			out << "\n";
+		}
+		return out;
 	}
 
 
 private:
-	int hash(int key, int i){
+	int hash(int key){
 		//h(k, i) = f(k) + p(i) % M
-		return ((key + i) % CAPACITY);
+		return ((key) % CAPACITY);
 	}
 }
 ;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "Hashmap.h"
+#include "HashmapBucket.h"
 #include "catch/include/catch.hpp"
 
 
@@ -45,3 +46,31 @@ TEST_CASE("ADDING/REMOVING", "ADDING AND REMOVING FROM HASH, FILL AND EMPTY"){
 
 }
 
+
+TEST_CASE("ADDING/REMOVING BUCKETS", "ADDING AND REMOVING FROM HASH BUCKETS, FILL AND EMPTY"){
+	SECTION("BUCKETS"){
+		HashmapBucket bucket = HashmapBucket(5);
+		REQUIRE(bucket.insert(0, '0'));
+		REQUIRE(bucket.insert(1, '1'));
+		REQUIRE(bucket.insert(114, '2'));		
+		REQUIRE(bucket.insert(14, '3'));
+		REQUIRE(bucket.insert(1, '4'));
+		REQUIRE(bucket.insert(13, '5'));		
+		REQUIRE(bucket.insert(4, '6'));
+		REQUIRE(bucket.insert(16, '7'));
+		REQUIRE(bucket.insert(3, '8'));		
+		REQUIRE(bucket.insert(10, '9'));
+		REQUIRE(bucket.insert(11, 'a'));
+		REQUIRE(bucket.insert(12, 'b'));		
+		REQUIRE(bucket.insert(3442, 'c'));
+		REQUIRE(bucket.insert(26, 'd'));
+		REQUIRE(bucket.insert(4, 'e'));
+		char test;
+		REQUIRE(bucket.remove(16, test));
+		std::cout << test << std::endl;
+		bucket.print(std::cout);
+	}
+
+
+
+}
